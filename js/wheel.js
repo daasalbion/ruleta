95d34065748226c5e629837f0d63bd7c9866d265
augusto <augusto@entermovil.com.py>
2014-05-20 09:17:39 -0400

    ya anda quedarse en un lugar especifico

--- js/wheel.js
+++ js/wheel.js
@@ -200,7 +200,6 @@ var wheel = {
         if(tiempo == 0){
 
             tiempo = 5*10;
-
         }
 
         console.log("tiempo: " + tiempo);
@@ -349,7 +348,7 @@ window.onload = function() {
 $(document).ready(function(){
 
     $('#confirmar_valor').click(
-        function(){
+        function(){      //0  1  2  3  4  5  6  7   8   9
             var angulos = [9, 8, 7, 6, 5, 4, 3 ,2 , 1 , 0]
             //wheel.valorEsperado = angulos[$('#valor_esperado').val()];
             wheel.valorEsperado = $('#valor_esperado').val();
